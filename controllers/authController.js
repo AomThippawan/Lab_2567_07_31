206cf3416eb192a7494f992e8d3c59639da87549
AomThippawan <157473472+AomThippawan@users.noreply.github.com>
2024-10-13 21:00:00 +0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,6 +11,7 @@ exports.register = async (req, res) => {
         const user = new User({ username, password: hashedPassword, name, role });
         await user.save();
         res.status(201).send("User registered");
+        console.log({ user })
     } catch (err) {
         res.status(400).send(err.message);
     }
@@ -21,19 +22,22 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
     const { username, password } = req.body;
     try {
-        const user = await User.findOne({ username });
-        if (!user) return res.status(400).send("User not found");
-        const isMatch = await bcrypt.compare(password, user.password);
+        const users = await User.findOne({ username });
+        if (!users) return res.status(400).send("User not found");
+        const isMatch = await bcrypt.compare(password, users.password);
         if (!isMatch) return res.status(400).send("Invalid credentials");
 
+        //add don't show password
+        const user = await User.findOne({username}).select("-password");
+        
         const accessToken = jwt.sign(
-            { userId: user._id },
+            { userId: users._id },
             process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: "5m" }
+            { expiresIn: "3h" }
         );
 
         const refreshToken = jwt.sign(
-            { userId: user._id },
+            { userId: users._id },
             process.env.REFRESH_TOKEN_SECRET
         );
         res.json({ user, accessToken, refreshToken });
